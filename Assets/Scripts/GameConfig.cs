6e91a907f94f2e850731e1e9a83b6a08edcd2b9f
kev-fbx <ktran041203@gmail.com>
2026-03-14 23:15:13 +1100

    Attempt to tart on global config/game content system

---
+++ Assets/Scripts/GameConfig.cs
@@ -0,0 +1,8 @@
+﻿// idk if we'll use
+public static class GameConfig
+{
+    public const int NumRounds = 10;
+    public const int TargetProfit = 5000000;
+    public const int ResumesPerRound = 10;
+    public const int GlobalApplicantPool = 100;
+}
--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,7 +1,11 @@
+using System;
+
 public static class Globals
 {
-    public enum DepartmentType
+
+    public enum Department
     {
+        None,
         FrontDesk,
         Replenishment,
         Garden,
@@ -18,5 +22,45 @@ public static class Globals
         Strength,
         Intelligence,
         Charisma
+    }
+
+    public enum ObservingEvent
+    {
+        Fight, DptBuff, DptDebuff
+    }
+
+
+    [Serializable]
+    public class Worker
+    {
+        public int id;
+        public string name;
+        public PersonalityType personality;
+        public float salary;
+        public float strength;
+        public float intelligence;
+        public float charisma;
+
+        public Department assignedDepartment = Department.None;
+        public float earningsMultiplier = 1f;
+        public bool isFighting = false;
+        public bool isFired = false;
+    }
+
+    [Serializable]
+    public class FightEvent
+    {
+        public ObservingEvent eventType = ObservingEvent.Fight;
+        public Worker initiator;
+        public Worker target;
+        public Department department;
+    }
+
+    [Serializable]
+    public class DepartmentEvent
+    {
+        public ObservingEvent eventType;
+        public Department department;
+        public float statMultiplier;
     }
 }
