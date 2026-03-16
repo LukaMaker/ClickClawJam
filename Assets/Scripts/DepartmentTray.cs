d9c4777b85083bc0a4256ebf21fb88749b05a21e
lukamaker <lbskvc@gmail.com>
2026-03-16 21:45:29 +1100

    tray highlight

--- Assets/Scripts/DepartmentTray.cs
+++ Assets/Scripts/DepartmentTray.cs
@@ -4,11 +4,23 @@ public class DepartmentTray : MonoBehaviour
 {
     public BaseDepartment department;
     public bool isShred, isRecycle;
+    [SerializeField] private Material hightlight, normal;
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Resume>())
         {
             other.GetComponent<Resume>().currentTray = this;
+        }
+    }
+    private void FixedUpdate()
+    {
+        GetComponent<MeshRenderer>().material = normal;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Resume>())
+        {
+            GetComponent<MeshRenderer>().material = hightlight;
         }
     }
     private void OnTriggerExit(Collider other)
