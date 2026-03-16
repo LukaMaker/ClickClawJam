c7fe6d49025b72ba8fca5de17c468f157f46bb56
outerhuman-noise <ethan.chuen@gmail.com>
2026-03-16 16:19:06 +1100

    character portrait builder

--- Assets/Scripts/CharacterPortrait.cs
+++ Assets/Scripts/CharacterPortrait.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class CharacterPortrait : MonoBehaviour
 {
     [Header("Part Renderers")]
-    public SpriteRenderer body;
-    public SpriteRenderer mouth;
-    public SpriteRenderer nose;
-    public SpriteRenderer hair;
-    public SpriteRenderer accessory;
+    [SerializeField] private SpriteRenderer body;
+    [SerializeField] private SpriteRenderer mouth;
+    [SerializeField] private SpriteRenderer nose;
+    [SerializeField] private SpriteRenderer hair;
+    [SerializeField] private SpriteRenderer accessory;
 
     [Header("Hair Colour")]
     public Color[] hairColours;
+
+    public void BuildPortrait(Employee employee)
+    {
+        body.sprite =       employee.body;
+        mouth.sprite =      employee.mouth;
+        nose.sprite =       employee.nose;
+        hair.sprite =       employee.hair;
+        accessory.sprite =  employee.accessory;
+    }
 }
